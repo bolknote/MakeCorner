3191e4c93cf51a168955522026ab44b645025ea6
Evgeny Stepanischev <imbolk@gmail.com>
2011-05-26 19:20:33 +0400

    Синтаксическая ошибка

--- maxproc_windows.go
+++ maxproc_windows.go
@@ -3,7 +3,7 @@ package main
 import "syscall"
 import "runtime"
 
-// выставляем количество потоков = количествую процессоров
+// выставляем количество потоков = количеству процессоров
 func setmaxprocs() {
     if n, _ := strconv.Atoi(os.Getenv("NUMBER_OF_PROCESSORS"); n > 0 {
         runtime.GOMAXPROCS(n)
